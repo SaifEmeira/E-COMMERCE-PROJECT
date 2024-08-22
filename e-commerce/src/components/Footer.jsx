846265077809dce2ec9371cb7b3a147fcfc569dd
SaifEmeira <saifemeira@gmail.com>
2024-08-22 19:26:38 +0300

    fix

--- e-commerce/src/components/Footer.jsx
+++ e-commerce/src/components/Footer.jsx
@@ -55,7 +55,7 @@ export default function Footer() {
     </div>
     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
     <div className="sm:flex sm:items-center sm:justify-between">
-      <span className="text-sm text-green-700 sm:text-center ">© 2024 <a  className=" hover:text-green-700 hover:underline cursor-pointer text-green-700 ">Flowbite™</a>. All Rights Reserved.
+      <span className="text-sm text-green-700 sm:text-center ">© 2025 <a  className=" hover:text-green-700 hover:underline cursor-pointer text-green-700 ">Flowbite™</a>. All Rights Reserved.
       </span>
       <div className="flex mt-4 sm:justify-center sm:mt-0">
         <a href="#" className="text-green-700">
